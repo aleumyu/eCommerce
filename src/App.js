ef6e8fcc1d3dc364680714de8218176743350935
Aleum Yang Urfels <aleum.yu@gmail.com>
2021-01-21 21:45:13 -0500

    feat: add a shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -3,19 +3,16 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/hompage/homepage';
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={HomePage}></Route>
-      <Route path="/hats" component={HatsPage}></Route>
-    </Switch>
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage}></Route>
+        <Route exact path="/shop" component={ShopPage}></Route>
+      </Switch>
+    </div>
   );
 }
 
---
+++ src/components/preview-collection/collection-preview.component.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import CollectionItem from '../collection-item/collection-item.component';
+import './collection-preview.style.scss';
+
+const CollectionPreview = ({ items, title }) => (
+  <div className="collection-preview">
+    <h1 className="title">{title.toUpperCase()}</h1>
+    <div className="preview">
+      {items
+        .filter((item, index) => index < 4)
+        .map(({ id, ...otherCollectionItems }) => (
+          <CollectionItem key={id} {...otherCollectionItems}></CollectionItem>
+        ))}
+    </div>
+  </div>
+);
+
+export default CollectionPreview;
